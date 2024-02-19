97ab5485428056a95ecf38d95b88266e054978e3
BALCHANDRA BACHANATTI <117996698+balcha95@users.noreply.github.com>
2024-02-19 20:04:08 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "jack.roper"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-03f4878755434977f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "tfcloud"
+  }
+}
